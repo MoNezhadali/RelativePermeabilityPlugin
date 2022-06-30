02aafea6e0347d625930db621000720c9f84fbfd
Viktoria Christine Vahlin (OG SUB RPE) <vvah@be-linrgsn154.be.statoil.no>
2022-06-30 15:06:53 +0200

    change names in plugin.py

--- code/RelativePermeability/plugins/relative_permeability/_plugin.py
+++ code/RelativePermeability/plugins/relative_permeability/_plugin.py
@@ -13,35 +13,37 @@ from ._plugin_ids import PlugInIDs # importing the namespace
 from .shared_settings import Filter
 
 class RelativePermeability(WebvizPluginABC):
-    def __init__(self, path_to_population_data_csv_file: Path ) -> None:
+    def __init__(self, path_to_relpermfile: Path ) -> None:
         super().__init__(stretch=True) # super refer to class inhereted from, init from ABC
 
         # set a member, self first for all
         self.error_message = "" 
 
+        # when reading from file must check that these are the keywords, if not raise ValueError
+
         try:
-            self.population_df = pd.read_csv(path_to_population_data_csv_file) # df = data frame
+            self.relperm_df = pd.read_csv(path_to_relpermfile) # df = data frame
         except PermissionError:
             self.error_message = (
-                f"Access to file '{path_to_population_data_csv_file}' denied. "
-                "Please check your path for 'path_to_population_data_csv_file' and make sure you have access to it."
+                f"Access to file '{path_to_relpermfile}' denied. "
+                f"Please check your path for '{path_to_relpermfile}' and make sure you have access to it."
             )
             return
         except FileNotFoundError:
             self.error_message = (
-                f"The file {path_to_population_data_csv_file}' not found."
+                f"The file {path_to_relpermfile}' not found."
                 "Please check you path"
             )
             return
         except pd.errors.ParserError:
             self.error_message = (
-                f"The file '{path_to_population_data_csv_file}' is not a valid csv file."
+                f"The file '{path_to_relpermfile}' is not a valid csv file."
             )
         except pd.errors.EmptyDataError:
             self.error_message = (
-                f"The file '{path_to_population_data_csv_file}' is an empty file."
+                f"The file '{path_to_relpermfile}' is an empty file."
             )
         except Exception:
             self.error_message = (
-                f"Unknown exception when trying to read '{path_to_population_data_csv_file}"
+                f"Unknown exception when trying to read '{path_to_relpermfile}"
             )
